5f8258226786657ddf025a04f716b1c49c4639a6
Enrique Bonifacio <enriqueboni80@hotmail.com>
2021-08-23 18:21:24 -0300

    Implementando Calculadora

---
+++ RestWithAPSNETUdemy/RestWithAPSNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithAPSNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public CalculatorController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber + secondNumber;
+            return Ok(result.ToString());
+        }
+    }
+}
